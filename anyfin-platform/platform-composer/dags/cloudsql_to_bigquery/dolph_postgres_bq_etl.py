5affe205c5378fcba220c0cdf92036ad3aeb13be
Charalampos Georgiadis <csioa@users.noreply.github.com>
2021-05-04 14:33:54 +0200

    Update main dolph etl (#419)
    * Update main etl with new pg_bq_template etl
    
    * Update dolph etl pipeline to use pg_bq template
    
    * Updates in main etl
    
    * Add checks in dolph etl

--- anyfin-platform/platform-composer/dags/cloudsql_to_bigquery/dolph_postgres_bq_etl.py
+++ anyfin-platform/platform-composer/dags/cloudsql_to_bigquery/dolph_postgres_bq_etl.py
@@ -1,25 +1,26 @@
 import os
-import json
-from datetime import datetime, timedelta
-import tempfile
+from datetime import datetime, timedelta, date
+
 from airflow import DAG
 from airflow.models import Variable
 from airflow.contrib.operators.bigquery_check_operator import BigQueryCheckOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
 from airflow.utils.trigger_rule import TriggerRule
 from airflow.contrib.operators.bigquery_operator import BigQueryOperator
-from airflow.contrib.hooks.gcs_hook import GoogleCloudStorageHook
-from airflow.hooks.postgres_hook import PostgresHook
+
+from airflow.operators.dummy_operator import DummyOperator
 from DataFlowPython3Operator import DataFlowPython3Operator
 
+from cloudsql_to_bigquery.utils.etl_utils import ETL
+
 PROJECT_NAME = 'anyfin'
-
 GCS_BUCKET = 'sql-to-bq-etl'
+DATABASE_NAME = 'dolph'
 
-TEMPLATE_FILE = os.path.dirname(os.path.realpath(__file__)) + '/beam_utils/dolph_etl.py'
+TEMPLATE_FILE = os.path.dirname(os.path.realpath(__file__)) + '/beam_utils/pg_bq_etl.py'
 SETUP_FILE = os.path.dirname(os.path.realpath(__file__)) + '/beam_utils/setup.py'
-with open(os.path.join(os.path.dirname(__file__), 'pg_schemas/dolph_schemas_state.json')) as f:
-    TABLES = json.loads(f.read())
+
+ETL = ETL(GCS_BUCKET=GCS_BUCKET, DATABASE_NAME=DATABASE_NAME)
 
 default_args = {
     'owner': 'ds-anyfin',
@@ -33,7 +34,7 @@ default_args = {
 }
 
 dag = DAG(
-    'dolph_postgres_bq_etl',
+    f'{DATABASE_NAME}_postgres_bq_etl',
     default_args=default_args,
     catchup=False,
     schedule_interval='0 2,9,11 * * *',
@@ -41,104 +42,16 @@ dag = DAG(
     concurrency=3
 )
 
-
-def deduplication_success(**context):
-    return True
-
-
-# Extract a list of tables seperated by if they have been updated or not
-# Return: Dictionary with extractable tables and tables containing unknown columns
-def extract_tables(bucket_json_schemata = TABLES):
-    # DB connection
-    con = PostgresHook(postgres_conn_id='dolph_replica').get_conn()
-    cur = con.cursor()
-
-    # Fetch current state of tables
-    schema_query = '''
-        SELECT
-           table_name,
-           json_agg(concat(column_name::text, ':', data_type::text)) as columns
-        FROM
-           information_schema.columns
-        WHERE
-           table_name in {tables}
-        GROUP BY table_name
-        ORDER BY 1;
-    '''.format(tables="('{}')".format("','".join([t for t in bucket_json_schemata])))
-    cur.execute(schema_query)
-    db_state_schemata = cur.fetchall()
-
-    # Close DB connection
-    cur.close()
-    con.close()
-
-    missing_columns = {}
-
-    for (db_table, db_columns) in db_state_schemata:
-        bucket_table_columns = bucket_json_schemata.get(db_table).get('schema')
-
-        db_column_names = [col.split(':')[0] for col in db_columns]
-        bucket_column_names = bucket_table_columns.keys()
-
-        discrepency = list(set(db_column_names) - set(bucket_column_names)) + \
-                  list(set(bucket_column_names) - set(db_column_names))
-
-        if not discrepency:
-            continue
-        else:
-            missing_columns[db_table] = discrepency
-
-    tables_to_extract = [t for t in bucket_json_schemata if t not in list(missing_columns)]
-
-    all_tables = {}
-    all_tables['tables_to_extract'] = tables_to_extract # List of tables
-    all_tables['missing_columns'] = missing_columns     # Dict with table: column
-
-    return all_tables
-
-
-def no_missing_columns(**context):
-
-    # Fetches task instance from context and pulls the variable from xcom
-    missing_columns = context['ti'].xcom_pull(task_ids='extract_tables')['missing_columns']
-
-    # Check if dictionary is empty
-    if not(missing_columns):
-        return True
-    else:
-        raise ValueError('These columns are either missing or they have changed type: ', str(missing_columns))
-
-
-def upload_table_names(**context):
-    # Fetches task instance from context and pulls the variable from xcom
-    dict_tables = context['ti'].xcom_pull(task_ids='extract_tables')
-    json_tables = json.dumps(dict_tables)
-
-    # Connect to GCS
-    gcs_hook = GoogleCloudStorageHook(google_cloud_storage_conn_id='postgres-bq-etl-con')
-
-    # Create a temporary JSON file and upload it to GCS
-    with tempfile.NamedTemporaryFile(mode="w") as file:
-        file.write(json_tables)
-        file.flush()
-        gcs_hook.upload(
-            bucket=GCS_BUCKET,
-            object='dolph_table_info.json',
-            mime_type='application/json',
-            filename=file.name
-        )
-
-
 task_extract_tables = PythonOperator(
     task_id='extract_tables',
-    python_callable=extract_tables,
+    python_callable=ETL.extract_tables,
     xcom_push=True,
     dag=dag
 )
 
 task_no_missing_columns = PythonOperator(
     task_id='no_missing_columns',
-    python_callable=no_missing_columns,
+    python_callable=ETL.no_missing_columns,
     provide_context=True,
     retries=0,
     dag=dag
@@ -146,7 +59,7 @@ task_no_missing_columns = PythonOperator(
 
 task_upload_result_to_gcs = PythonOperator(
     task_id='upload_result_to_gcs',
-    python_callable=upload_table_names,
+    python_callable=ETL.upload_table_names,
     provide_context=True,
     retries=2,
     dag=dag
@@ -155,7 +68,7 @@ task_upload_result_to_gcs = PythonOperator(
 extract_from_cloudsql = DataFlowPython3Operator(
     task_id='extract_from_cloudsql',
     py_file=TEMPLATE_FILE,
-    job_name='dolph-etl',
+    job_name=f'{DATABASE_NAME}-etl',
     provide_context=True,
     dataflow_default_options={
         "project": PROJECT_NAME,
@@ -166,7 +79,8 @@ extract_from_cloudsql = DataFlowPython3Operator(
         "experiment": "use_beam_bq_sink",
         "date": '{{ds}}',
         "setup_file": SETUP_FILE,
-        "temp_location": f'gs://{GCS_BUCKET}/Temp/'
+        "temp_location": f'gs://{GCS_BUCKET}/Temp/',
+        "database_name": f"{DATABASE_NAME}"
     },
     delegate_to="postgres-bq-etl@anyfin.iam.gserviceaccount.com",
     options={
@@ -177,8 +91,42 @@ extract_from_cloudsql = DataFlowPython3Operator(
     dag=dag
 )
 
+first_daily_run = BranchPythonOperator(
+    task_id='first_daily_run',
+    provide_context=True,
+    python_callable=ETL.check_if_first_daily_run,
+    dag=dag
+)
+
+postgres_status = PythonOperator(
+    task_id='postgres_status',
+    provide_context=True,
+    python_callable=ETL.fetch_postgres_rowcount,
+    dag=dag
+)
+
+bq_status = PythonOperator(
+    task_id='bq_status',
+    provide_context=True,
+    python_callable=ETL.fetch_bigquery_rowcount,
+    dag=dag
+)
+
+check_postgres_against_bq = PythonOperator(
+    task_id='check_postgres_against_bq',
+    provide_context=True,
+    python_callable=ETL.bq_pg_comparison,
+    email_on_failure=True,
+    dag=dag
+)
+
+no_check = DummyOperator(
+    task_id='no_check',
+    dag=dag
+)
+
 dedup_tasks = []
-for table in [t for t in TABLES if t != 'transactions']:
+for table in [t for t in ETL.get_tables() if t != 'transactions']:
     dedup = BigQueryOperator(
         task_id='deduplicate_' + table,
         sql=f"""
@@ -186,13 +134,13 @@ for table in [t for t in TABLES if t != 'transactions']:
                 select
                     id,
                     max(_ingested_ts) as max_ingested_ts
-                from anyfin.dolph_staging.{table}_raw group by 1
+                from anyfin.{DATABASE_NAME}_staging.{table}_raw group by 1
             )
             select
                 t.*
             from temp join
-                anyfin.dolph_staging.{table}_raw t on temp.id= t.id and temp.max_ingested_ts=t._ingested_ts""",
-        destination_dataset_table=f"anyfin.dolph.{table}",
+                anyfin.{DATABASE_NAME}_staging.{table}_raw t on temp.id= t.id and temp.max_ingested_ts=t._ingested_ts""",
+        destination_dataset_table=f"anyfin.{DATABASE_NAME}.{table}",
         cluster_fields=['id'],
         time_partitioning = {'field': 'created_at'},
         use_legacy_sql=False,
@@ -206,7 +154,7 @@ for table in [t for t in TABLES if t != 'transactions']:
 
 deduplication_success_confirmation = PythonOperator(
     task_id='deduplication_success_confirmation',
-    python_callable=deduplication_success,
+    python_callable=ETL.deduplication_success,
     provide_context=True,
     email_on_failure=True,
     trigger_rule=TriggerRule.ALL_SUCCESS,
@@ -215,13 +163,13 @@ deduplication_success_confirmation = PythonOperator(
 
 all_tables_updated = BigQueryCheckOperator(
     task_id='all_tables_updated',
-    sql='''
+    sql=f'''
     SELECT COUNTIF(is_up_to_date)/COUNT(table_id) = 1 all_up_to_date FROM (
         SELECT
           table_id,
           '{{tomorrow_ds}}' <= DATE(TIMESTAMP_MILLIS(last_modified_time)) is_up_to_date
         FROM
-        `anyfin.dolph_staging.__TABLES__`)
+        `anyfin.{DATABASE_NAME}_staging.__TABLES__`)
     ''',
     bigquery_conn_id='bigquery_default',
     use_legacy_sql=False,
@@ -231,6 +179,11 @@ all_tables_updated = BigQueryCheckOperator(
 task_extract_tables >> task_no_missing_columns
 
 task_extract_tables >> task_upload_result_to_gcs
+
+extract_from_cloudsql >> first_daily_run
+
+first_daily_run >> postgres_status >> bq_status >> check_postgres_against_bq
+first_daily_run >> no_check
 
 task_upload_result_to_gcs >> extract_from_cloudsql >> dedup_tasks
 
--- anyfin-platform/platform-composer/dags/cloudsql_to_bigquery/main_postgres_bq_etl.py
+++ anyfin-platform/platform-composer/dags/cloudsql_to_bigquery/main_postgres_bq_etl.py
@@ -1,28 +1,26 @@
 import os
-import json
 from datetime import datetime, timedelta, date
-import tempfile
-import logging
-from google.cloud import bigquery
-from airflow import DAG, AirflowException
+
+from airflow import DAG
 from airflow.models import Variable
 from airflow.contrib.operators.bigquery_check_operator import BigQueryCheckOperator
 from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
 from airflow.utils.trigger_rule import TriggerRule
-from airflow.contrib.operators.bigquery_operator import BigQueryOperator
-from airflow.contrib.hooks.gcs_hook import GoogleCloudStorageHook
-from airflow.hooks.postgres_hook import PostgresHook
+from airflow.contrib.operators.bigquery_operator import BigQueryCheckOperator
+
 from airflow.operators.dummy_operator import DummyOperator
 from DataFlowPython3Operator import DataFlowPython3Operator
 
+from cloudsql_to_bigquery.utils.etl_utils import ETL
+
 PROJECT_NAME = 'anyfin'
-
 GCS_BUCKET = 'sql-to-bq-etl'
+DATABASE_NAME = 'main'
 
-TEMPLATE_FILE = os.path.dirname(os.path.realpath(__file__)) + '/beam_utils/main_etl.py'
+TEMPLATE_FILE = os.path.dirname(os.path.realpath(__file__)) + '/beam_utils/pg_bq_etl.py'
 SETUP_FILE = os.path.dirname(os.path.realpath(__file__)) + '/beam_utils/setup.py'
-with open(os.path.join(os.path.dirname(__file__), 'pg_schemas/main_schemas_state.json')) as f:
-    TABLES = json.loads(f.read())
+
+ETL = ETL(GCS_BUCKET=GCS_BUCKET, DATABASE_NAME=DATABASE_NAME)
 
 default_args = {
     'owner': 'ds-anyfin',
@@ -36,7 +34,7 @@ default_args = {
 }
 
 dag = DAG(
-    'main_postgres_bq_etl',
+    f'{DATABASE_NAME}_postgres_bq_etl',
     default_args=default_args,
     catchup=False,
     schedule_interval='0 2,9,11 * * *',
@@ -44,110 +42,16 @@ dag = DAG(
     concurrency=3
 )
 
-
-def deduplication_success(**context):
-    return True
-
-# Extract a list of tables seperated by if they have been updated or not
-# Return: Dictionary with extractable tables and tables containing unknown columns
-
-
-def extract_tables(bucket_json_schemata=TABLES):
-    # DB connection
-    con = PostgresHook(postgres_conn_id='main_replica').get_conn()
-    cur = con.cursor()
-
-    # Fetch current state of tables
-    schema_query = '''
-        SELECT
-           table_name,
-           json_agg(concat(column_name::text, ':', data_type::text)) as columns
-        FROM
-           information_schema.columns
-        WHERE
-           table_name in {tables}
-        GROUP BY table_name
-        ORDER BY 1;
-    '''.format(tables="('{}')".format("','".join([t for t in bucket_json_schemata])))
-    cur.execute(schema_query)
-    db_state_schemata = cur.fetchall()
-
-    # Close DB connection
-    cur.close()
-    con.close()
-
-    missing_columns = {}
-
-    for (db_table, db_columns) in db_state_schemata:
-        bucket_table_columns = bucket_json_schemata.get(db_table).get('schema')
-
-        db_column_names = [col.split(':')[0] for col in db_columns]
-        bucket_column_names = bucket_table_columns.keys()
-
-        discrepency = list(set(db_column_names) - set(bucket_column_names)) + \
-            list(set(bucket_column_names) - set(db_column_names))
-
-        if not discrepency:
-            continue
-        else:
-            missing_columns[db_table] = discrepency
-
-    tables_to_extract = [
-        t for t in bucket_json_schemata if t not in list(missing_columns)]
-
-    all_tables = {}
-    all_tables['tables_to_extract'] = tables_to_extract  # List of tables
-    # Dict with table: column
-    all_tables['missing_columns'] = missing_columns
-
-    return all_tables
-
-
-def no_missing_columns(**context):
-
-    # Fetches task instance from context and pulls the variable from xcom
-    missing_columns = context['ti'].xcom_pull(
-        task_ids='extract_tables')['missing_columns']
-
-    # Check if dictionary is empty
-    if not(missing_columns):
-        return True
-    else:
-        raise ValueError(
-            'These columns are either missing or they have changed type: ', str(missing_columns))
-
-
-def upload_table_names(**context):
-    # Fetches task instance from context and pulls the variable from xcom
-    dict_tables = context['ti'].xcom_pull(task_ids='extract_tables')
-    json_tables = json.dumps(dict_tables)
-
-    # Connect to GCS
-    gcs_hook = GoogleCloudStorageHook(
-        google_cloud_storage_conn_id='postgres-bq-etl-con')
-
-    # Create a temporary JSON file and upload it to GCS
-    with tempfile.NamedTemporaryFile(mode="w") as file:
-        file.write(json_tables)
-        file.flush()
-        gcs_hook.upload(
-            bucket=GCS_BUCKET,
-            object='main_table_info.json',
-            mime_type='application/json',
-            filename=file.name
-        )
-
-
 task_extract_tables = PythonOperator(
     task_id='extract_tables',
-    python_callable=extract_tables,
+    python_callable=ETL.extract_tables,
     xcom_push=True,
     dag=dag
 )
 
 task_no_missing_columns = PythonOperator(
     task_id='no_missing_columns',
-    python_callable=no_missing_columns,
+    python_callable=ETL.no_missing_columns,
     provide_context=True,
     retries=0,
     dag=dag
@@ -155,7 +59,7 @@ task_no_missing_columns = PythonOperator(
 
 task_upload_result_to_gcs = PythonOperator(
     task_id='upload_result_to_gcs',
-    python_callable=upload_table_names,
+    python_callable=ETL.upload_table_names,
     provide_context=True,
     retries=2,
     dag=dag
@@ -164,7 +68,7 @@ task_upload_result_to_gcs = PythonOperator(
 extract_from_cloudsql = DataFlowPython3Operator(
     task_id='extract_from_cloudsql',
     py_file=TEMPLATE_FILE,
-    job_name='main-etl',
+    job_name=f'{DATABASE_NAME}-etl',
     provide_context=True,
     dataflow_default_options={
         "project": PROJECT_NAME,
@@ -176,7 +80,9 @@ extract_from_cloudsql = DataFlowPython3Operator(
         "date": '{{ds}}',
         "machine_type": "n1-standard-4",
         "setup_file": SETUP_FILE,
-        "temp_location": f'gs://{GCS_BUCKET}/Temp/'
+        "temp_location": f'gs://{GCS_BUCKET}/Temp/',
+        "database_name": f"{DATABASE_NAME}"
+
     },
     delegate_to="postgres-bq-etl@anyfin.iam.gserviceaccount.com",
     options={
@@ -187,85 +93,32 @@ extract_from_cloudsql = DataFlowPython3Operator(
     dag=dag
 )
 
-def check_if_first_daily_run(ds, **kwargs):
-    today = date.today()
-    today = today.strftime("%Y-%m-%d")
-    if today != ds:
-        return 'postgres_status'
-    else:
-        return 'no_check'
-
 
 first_daily_run = BranchPythonOperator(
     task_id='first_daily_run',
     provide_context=True,
-    python_callable=check_if_first_daily_run,
+    python_callable=ETL.check_if_first_daily_run,
     dag=dag
 )
-
-def fetch_postgres_rowcount(tables, ds, **kwargs):
-    db = PostgresHook('main_replica')
-    query = []
-    for table_name in tables:
-        query.append(f"SELECT '{table_name}', COUNT(id) FROM {table_name} where created_at::date = '{ds}'")
-    query = ' UNION ALL '.join(query)
-    
-    rowcounts = db.get_records(query)
-    counts = {}
-    for row in rowcounts:
-        counts[row[0]] = row[1]
-    return counts
 
 postgres_status = PythonOperator(
     task_id='postgres_status',
     provide_context=True,
-    op_kwargs={'tables': TABLES},
-    python_callable=fetch_postgres_rowcount,
+    python_callable=ETL.fetch_postgres_rowcount,
     dag=dag
 )
-
-def fetch_bigquery_rowcount(tables, ds, **kwargs):
-    query = []
-    for table_name in tables:
-        query.append(f"SELECT '{table_name}' table_name, COUNT(DISTINCT id) num_of_unique_rows FROM `anyfin.main_staging.{table_name}_raw` where DATE(created_at) = '{ds}'")
-    query = ' UNION ALL '.join(query)
-    client = bigquery.Client()
-    query_job = client.query(query)
-    results = query_job.result()
-    counts = {}
-    for row in results:
-        counts[row.table_name] = row.num_of_unique_rows
-    return counts
 
 bq_status = PythonOperator(
     task_id='bq_status',
     provide_context=True,
-    op_kwargs={'tables': TABLES},
-    python_callable=fetch_bigquery_rowcount,
+    python_callable=ETL.fetch_bigquery_rowcount,
     dag=dag
 )
-
-def bq_pg_comparison(tables, **kwargs):
-    postgres_results = kwargs['ti'].xcom_pull(task_ids='postgres_status')
-    bq_results = kwargs['ti'].xcom_pull(task_ids='bq_status')
-
-    discrepancies = {}
-
-    for table_name in tables:
-        logging.info(f"Table: {table_name}: Postgres - {postgres_results[table_name]} || BQ - {bq_results[table_name]}")
-        if postgres_results[table_name] != bq_results[table_name]:
-            discrepancies[table_name] = {'postgres': postgres_results[table_name], 'bq': bq_results[table_name]}
-    
-    if not discrepancies:
-        return True
-    else:
-        raise AirflowException(f"Discrepancies found in tables: {' ,'.join(discrepancies.keys())}")
 
 check_postgres_against_bq = PythonOperator(
     task_id='check_postgres_against_bq',
     provide_context=True,
-    op_kwargs={'tables': TABLES},
-    python_callable=bq_pg_comparison,
+    python_callable=ETL.bq_pg_comparison,
     email_on_failure=True,
     dag=dag
 )
@@ -276,7 +129,7 @@ no_check = DummyOperator(
 )
 
 dedup_tasks = []
-for table in TABLES:
+for table in ETL.get_tables():
     if table == 'assessments':
         dedup = BigQueryOperator(
             task_id='deduplicate_' + table,
@@ -285,7 +138,7 @@ for table in TABLES:
                     SELECT 
                         id, 
                         max(_ingested_ts) as max_ingested_ts 
-                    FROM anyfin.main_staging.assessments_raw group by 1
+                    FROM anyfin.{DATABASE_NAME}_staging.assessments_raw group by 1
                 )
                 SELECT 
                     t.*,
@@ -321,9 +174,9 @@ for table in TABLES:
                     json_extract(main_policy,  '$.data.AsiakastietoCcisLookup' )  as `asiakastieto_ccis_lookup`,
                     json_extract(main_policy,  '$.data.KalpNew' )  as `kalp_new`
                     from temp join 
-                    anyfin.main_staging.{table}_raw t on temp.id= t.id and temp.max_ingested_ts=t._ingested_ts
+                    anyfin.{DATABASE_NAME}_staging.{table}_raw t on temp.id= t.id and temp.max_ingested_ts=t._ingested_ts
             """,
-            destination_dataset_table=f"anyfin.main.{table}",
+            destination_dataset_table=f"anyfin.{DATABASE_NAME}.{table}",
             cluster_fields=['id'],
             time_partitioning={'field': 'created_at'},
             use_legacy_sql=False,
@@ -342,13 +195,13 @@ for table in TABLES:
                     select 
                         id, 
                         max(_ingested_ts) as max_ingested_ts 
-                    from anyfin.main_staging.{table}_raw group by 1
+                    from anyfin.{DATABASE_NAME}_staging.{table}_raw group by 1
                 )
                 select 
                     t.* 
                 from temp join 
-                    anyfin.main_staging.{table}_raw t on temp.id= t.id and temp.max_ingested_ts=t._ingested_ts""",
-            destination_dataset_table=f"anyfin.main.{table}",
+                    anyfin.{DATABASE_NAME}_staging.{table}_raw t on temp.id= t.id and temp.max_ingested_ts=t._ingested_ts""",
+            destination_dataset_table=f"anyfin.{DATABASE_NAME}.{table}",
             cluster_fields=['id'],
             time_partitioning={'field': 'created_at'},
             use_legacy_sql=False,
@@ -360,10 +213,9 @@ for table in TABLES:
         dedup_tasks.append(dedup)
 
 
-
 deduplication_success_confirmation = PythonOperator(
     task_id='deduplication_success_confirmation',
-    python_callable=deduplication_success,
+    python_callable=ETL.deduplication_success,
     provide_context=True,
     email_on_failure=True,
     trigger_rule=TriggerRule.ALL_SUCCESS,
