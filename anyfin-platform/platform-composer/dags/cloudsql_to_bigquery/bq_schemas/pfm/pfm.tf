036489e760877232d259ba0f6634eb2cf8054a62
Malcolm Tivelius <malcolm@anyfin.com>
2021-06-04 14:05:28 +0200

    Add schemas to sync (#487)

--- anyfin-platform/platform-composer/dags/cloudsql_to_bigquery/bq_schemas/pfm/pfm.tf
+++ anyfin-platform/platform-composer/dags/cloudsql_to_bigquery/bq_schemas/pfm/pfm.tf
@@ -59,3 +59,120 @@ EOF
 }
 
 
+resource "google_bigquery_table" "customer_widgets_raw" {
+	dataset_id = "pfm_staging"
+	table_id   = "customer_widgets_raw"
+	project    = "anyfin"
+
+	labels = {
+		env = "default"
+	}
+
+	time_partitioning { 
+    	type = "DAY" 
+		field = "created_at" 
+	}
+
+	schema = <<EOF
+[
+	{
+		"mode": "NULLABLE",
+		"name": "id",
+		"type": "STRING"
+	},
+	{
+		"mode": "NULLABLE",
+		"name": "created_at",
+		"type": "TIMESTAMP"
+	},
+	{
+		"mode": "NULLABLE",
+		"name": "updated_at",
+		"type": "TIMESTAMP"
+	},
+	{
+		"mode": "NULLABLE",
+		"name": "customer_id",
+		"type": "INTEGER"
+	},
+	{
+		"mode": "NULLABLE",
+		"name": "type",
+		"type": "STRING"
+	},
+	{
+		"mode": "NULLABLE",
+		"name": "position",
+		"type": "INTEGER"
+	},
+	{
+		"mode": "NULLABLE",
+		"name": "_ingested_ts",
+		"type": "TIMESTAMP"
+	}
+]
+EOF
+}
+
+
+resource "google_bigquery_table" "salary_day_widgets_raw" {
+	dataset_id = "pfm_staging"
+	table_id   = "salary_day_widgets_raw"
+	project    = "anyfin"
+
+	labels = {
+		env = "default"
+	}
+
+	time_partitioning { 
+    	type = "DAY" 
+		field = "created_at" 
+	}
+
+	schema = <<EOF
+[
+	{
+		"mode": "NULLABLE",
+		"name": "id",
+		"type": "STRING"
+	},
+	{
+		"mode": "NULLABLE",
+		"name": "created_at",
+		"type": "TIMESTAMP"
+	},
+	{
+		"mode": "NULLABLE",
+		"name": "updated_at",
+		"type": "TIMESTAMP"
+	},
+	{
+		"mode": "NULLABLE",
+		"name": "customer_id",
+		"type": "INTEGER"
+	},
+	{
+		"mode": "NULLABLE",
+		"name": "color",
+		"type": "STRING"
+	},
+	{
+		"mode": "NULLABLE",
+		"name": "salary_day",
+		"type": "INTEGER"
+	},
+	{
+		"mode": "NULLABLE",
+		"name": "adjust_weekends",
+		"type": "BOOLEAN"
+	},
+	{
+		"mode": "NULLABLE",
+		"name": "_ingested_ts",
+		"type": "TIMESTAMP"
+	}
+]
+EOF
+}
+
+
