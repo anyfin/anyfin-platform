31e854b9b7da4e5cd0497e87273ac4d8b22e55bb
Malcolm Tivelius <malcolm@anyfin.com>
2022-04-01 15:11:50 +0100

    Add orchestration DAG for thrice daily models

---
+++ anyfin-platform/platform-composer/dags/run_thrice_daily.py
@@ -0,0 +1,50 @@
+from airflow import DAG
+from airflow.models import Variable
+from datetime import datetime, timedelta
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.utils.state import State
+
+
+default_args = {
+    'owner': 'ds-anyfin',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 4, 1),
+    'retries': 2,
+    'retry_delay': timedelta(minutes=10),
+    #'email_on_failure': True,
+    #'email_on_retry': False,
+    #'email': Variable.get('de_email', 'data-engineering@anyfin.com')
+}
+
+with DAG(
+    dag_id='run_thrice_daily', 
+    default_args=default_args, 
+    catchup=False,
+    description='This DAG is used to orcehstrate all of our DAGs that run thrice-daily',
+    #schedule_interval='0 2,9,11 * * *',
+    schedule_interval=None,
+    max_active_runs=1
+) as dag:
+
+
+    run_ETL = TriggerDagRunOperator(
+        task_id='run-postgres_bq_etl',
+        trigger_dag_id='postgres_bq_etl',
+        execution_date='{{ ds }}',
+        reset_dag_run=True,
+        wait_for_completion=True,
+        poke_interval=30,
+        allowed_states=[State.SUCCESS, State.FAILED],
+    )
+
+    run_dbt = TriggerDagRunOperator(
+        task_id='run-bigquery_dbt_models',
+        trigger_dag_id='bigquery_dbt_models',
+        execution_date='{{ ds }}',
+        reset_dag_run=True,
+        wait_for_completion=True,
+        poke_interval=30,
+        allowed_states=[State.SUCCESS, State.FAILED],
+    )
+
+run_ETL >> run_dbt
