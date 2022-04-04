fc3ecc40586ef9aef5c2070ef5608dd097f06129
Malcolm Tivelius <malcolm@anyfin.com>
2022-04-04 15:02:18 +0200

    de owner

--- anyfin-platform/platform-composer/dags/run_thrice_daily.py
+++ anyfin-platform/platform-composer/dags/run_thrice_daily.py
@@ -6,7 +6,7 @@ from airflow.utils.state import State
 
 
 default_args = {
-    'owner': 'ds-anyfin',
+    'owner': 'de-anyfin',
     'depends_on_past': False,
     'start_date': datetime(2022, 4, 1),
     'retries': 0,  # Do not retry as this will rerun all dbt models and ETLs again
