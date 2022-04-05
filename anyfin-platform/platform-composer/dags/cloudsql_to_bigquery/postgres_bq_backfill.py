22eb74073f345acbf1886beacc307f22c8677aa1
Malcolm Tivelius <malcolm@anyfin.com>
2022-04-05 10:59:12 +0200

    Tiny fix

--- anyfin-platform/platform-composer/dags/cloudsql_to_bigquery/postgres_bq_backfill.py
+++ anyfin-platform/platform-composer/dags/cloudsql_to_bigquery/postgres_bq_backfill.py
@@ -28,47 +28,47 @@ default_args = {
 	#'email': Variable.get('de_email', 'data-engineering@anyfin.com')
 }
 
-dag = DAG(
+with DAG(
 	'postgres_bq_backfill',
 	default_args=default_args,
 	catchup=False,
 	schedule_interval=None,
 	max_active_runs=1,
 	concurrency=12
-)
+) as dag:
 
-for DB in DATABASES_INFO:
-	DATABASE_NAME, INSTANCE_NAME = DB['DATABASE_NAME'], DB['INSTANCE_NAME']
-	backfill = BACKFILL(GCS_BUCKET=GCS_BUCKET, DATABASE_NAME=DATABASE_NAME)
-	if backfill.get_beam_tables():
-		# Operator to backfill tables with beam
-		beam_backfill = BeamRunPythonPipelineOperator(
-			task_id=f'{DATABASE_NAME}_beam_backfill',
-			runner="DataflowRunner",
-			py_file=TEMPLATE_FILE,
-			py_options=[],
-			py_requirements=[],
-			py_interpreter='python3',
-			py_system_site_packages=True,
-			pipeline_options={
-				"project": PROJECT_NAME,
-				"worker_zone": 'europe-west1-b',
-				"region": "europe-west1",
-				"staging_location": f'gs://{DATAFLOW_BUCKET}/Staging/',
-				"experiment": "use_beam_bq_sink",
-				"date": '{{ds}}',
-				"machine_type": "n1-standard-4",
-				"num-workers": '1',
-				"temp_location": f'gs://{DATAFLOW_BUCKET}/Temp/',
-				"database_name": f"{DATABASE_NAME}",
-				"setup_file": SETUP_FILE,
-				"poll_sleep": 30,
-			},
-			email_on_failure=True,
-			dataflow_config=DataflowConfiguration(
-				job_name=f'{DATABASE_NAME}-etl-backfill', 
-				project_id=PROJECT_NAME, 
-				location="europe-west1",
-				gcp_conn_id="dataflow-etl-connection"
+	for DB in DATABASES_INFO:
+		DATABASE_NAME, INSTANCE_NAME = DB['DATABASE_NAME'], DB['INSTANCE_NAME']
+		backfill = BACKFILL(GCS_BUCKET=GCS_BUCKET, DATABASE_NAME=DATABASE_NAME)
+		if backfill.get_beam_tables():
+			# Operator to backfill tables with beam
+			beam_backfill = BeamRunPythonPipelineOperator(
+				task_id=f'{DATABASE_NAME}_beam_backfill',
+				runner="DataflowRunner",
+				py_file=TEMPLATE_FILE,
+				py_options=[],
+				py_requirements=[],
+				py_interpreter='python3',
+				py_system_site_packages=True,
+				pipeline_options={
+					"project": PROJECT_NAME,
+					"worker_zone": 'europe-west1-b',
+					"region": "europe-west1",
+					"staging_location": f'gs://{DATAFLOW_BUCKET}/Staging/',
+					"experiment": "use_beam_bq_sink",
+					"date": '{{ds}}',
+					"machine_type": "n1-standard-4",
+					"num-workers": '1',
+					"temp_location": f'gs://{DATAFLOW_BUCKET}/Temp/',
+					"database_name": f"{DATABASE_NAME}",
+					"setup_file": SETUP_FILE,
+					"poll_sleep": 30,
+				},
+				email_on_failure=True,
+				dataflow_config=DataflowConfiguration(
+					job_name=f'{DATABASE_NAME}-etl-backfill', 
+					project_id=PROJECT_NAME, 
+					location="europe-west1",
+					gcp_conn_id="dataflow-etl-connection"
+				)
 			)
-		)
