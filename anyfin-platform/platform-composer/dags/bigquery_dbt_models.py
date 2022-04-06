ad71d0c1fcb6215913b3b36b320dadea0d7cd927
Malcolm Tivelius <malcolm@anyfin.com>
2022-04-06 12:45:45 +0200

    Use the dbt factory to create bigquery dbt models

--- anyfin-platform/platform-composer/dags/bigquery_dbt_models.py
+++ anyfin-platform/platform-composer/dags/bigquery_dbt_models.py
@@ -6,6 +6,7 @@ from airflow.providers.google.cloud.transfers.bigquery_to_bigquery import BigQue
 from airflow.operators.bash_operator import BashOperator
 import json
 import logging
+from utils.dbtTaskFactory import dbtTaskFactory
 
 
 DBT_DIR = '/home/airflow/gcs/dags/anyfin-data-model'
@@ -17,95 +18,43 @@ default_args = {
     'retries': 0,
     #'email_on_failure': True,
     #'email': Variable.get('de_email', 'data-engineering@anyfin.com'),
-    'start_date': datetime(2020, 11, 22),
+    'start_date': datetime(2022, 4, 1),
 }
 
 
-with DAG(
+dag = DAG(
     dag_id="bigquery_dbt_models", 
     default_args=default_args, 
     schedule_interval=None,  
     max_active_runs=1,
     catchup=False,
     concurrency=12
-) as dag:
+)
 
 
-    # task_export_intercom_diff = BigQueryToGCSOperator(
-    #     task_id="export_intercom_diff",
-    #     source_project_dataset_table="anyfin.product.intercom_diff_sync",
-    #     destination_cloud_storage_uris=[f"gs://anyfin-customer-facts/current-partition.json"],
-    #     export_format='NEWLINE_DELIMITED_JSON',
-    #     gcp_conn_id="bigquery-composer-connection",
-    #     dag=dag
-    # )
+task_export_intercom_diff = BigQueryToGCSOperator(
+    task_id="export_intercom_diff",
+    source_project_dataset_table="anyfin.product.intercom_diff_sync",
+    destination_cloud_storage_uris=[f"gs://anyfin-customer-facts/current-partition.json"],
+    export_format='NEWLINE_DELIMITED_JSON',
+    gcp_conn_id="bigquery-composer-connection",
+    dag=dag
+)
 
-    # task_save_intercom_previous_sync = BigQueryToBigQueryOperator(
-    #     task_id=f'save_intercom_previous_sync',
-    #     create_disposition='CREATE_IF_NEEDED',
-    #     write_disposition='WRITE_TRUNCATE',
-    #     source_project_dataset_tables=f'anyfin.product.intercom_sync',
-    #     destination_project_dataset_table='anyfin.product.intercom_previous_sync',
-    #     gcp_conn_id='bigquery-composer-connection',
-    #     dag=dag
-    # )
+task_save_intercom_previous_sync = BigQueryToBigQueryOperator(
+    task_id=f'save_intercom_previous_sync',
+    create_disposition='CREATE_IF_NEEDED',
+    write_disposition='WRITE_TRUNCATE',
+    source_project_dataset_tables=f'anyfin.product.intercom_sync',
+    destination_project_dataset_table='anyfin.product.intercom_previous_sync',
+    gcp_conn_id='bigquery-composer-connection',
+    dag=dag
+)
 
-    def load_manifest():
-        local_filepath = f"{DBT_DIR}/target/manifest.json"
-        with open(local_filepath) as f:
-            data = json.load(f)
+factory = dbtTaskFactory(DBT_DIR, dag, MODEL_TAG)
 
-        return data
-
-    def make_dbt_task(node, dbt_verb):
-        """Returns an Airflow operator either run and test an individual model"""
-        GLOBAL_CLI_FLAGS = "--no-write-json"
-        model = node.split(".")[-1]
-
-        if dbt_verb == "run":
-            dbt_task = BashOperator(
-                task_id=node, 
-                bash_command=f"dbt {GLOBAL_CLI_FLAGS} {dbt_verb} --project-dir {DBT_DIR} --profiles-dir {DBT_DIR} --target prod --select {model}"
-            )
-
-        return dbt_task
-
-    data = load_manifest()
-
-    dbt_tasks = {}
-    nodes = {node:value for node, value in data['nodes'].items() 
-            if node.split(".")[0] == "model" and MODEL_TAG in value["config"]["tags"]}
+dbt_tasks = factory.generate_tasks_from_manifest()
 
 
-    for node, node_info in nodes.items():
-        if node_info["config"]["materialized"] == "ephemeral":
-            continue
-        dbt_tasks[node] = make_dbt_task(node, "run")
-
-
-    for node, node_info in nodes.items():
-        if node_info["config"]["materialized"] == "ephemeral":
-            continue
-
-        # Set all model -> model dependencies
-        dependencies = node_info["depends_on"]["nodes"]
-        dependencies = list(set(dependencies))
-        while dependencies:
-            upstream_node = dependencies.pop()
-            
-            if not upstream_node in nodes:
-                continue
-
-            upstream_node_type = upstream_node.split(".")[0]
-            if upstream_node_type != "model":
-                continue
-            
-            if nodes[upstream_node]["config"]["materialized"] == "ephemeral":
-                dependencies += nodes[upstream_node]["depends_on"]["nodes"]
-                dependencies = list(set(dependencies))
-            else:
-                dbt_tasks[upstream_node] >> dbt_tasks[node]
-
-
-    #task_save_intercom_previous_sync >> dbt_tasks["model.anyfin_bigquery.intercom_sync"] 
-    #dbt_tasks["model.anyfin_bigquery.intercom_diff_sync"] >> task_export_intercom_diff
+task_save_intercom_previous_sync >> dbt_tasks["model.anyfin_bigquery.intercom_sync"] 
+dbt_tasks["model.anyfin_bigquery.intercom_diff_sync"] >> task_export_intercom_diff
