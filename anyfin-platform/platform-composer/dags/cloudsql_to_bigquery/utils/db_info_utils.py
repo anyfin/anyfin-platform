7b80cfe45683805805e0615284df85ee36a4e6ac
Malcolm Tivelius <malcolm@anyfin.com>
2022-05-30 19:37:43 +0100

    Add new database to list of databases
    Also add DESTINATION_PROJECT variable

--- anyfin-platform/platform-composer/dags/cloudsql_to_bigquery/utils/db_info_utils.py
+++ anyfin-platform/platform-composer/dags/cloudsql_to_bigquery/utils/db_info_utils.py
@@ -3,12 +3,13 @@
 # In order to dynamically generate tasks for all databases
 
 DATABASES_INFO = [
-	{'DATABASE_NAME': 'main', 'INSTANCE_NAME': 'anyfin-main-replica', 'DATABASE':'main'},
-	{'DATABASE_NAME': 'dolph', 'INSTANCE_NAME': 'anyfin-dolph-read-replica', 'DATABASE':'postgres'},
-	{'DATABASE_NAME': 'pfm', 'INSTANCE_NAME': 'pfm-replica', 'DATABASE':'postgres'},
-	{'DATABASE_NAME': 'psd2', 'INSTANCE_NAME': 'psd2-replica', 'DATABASE':'postgres'},
-	{'DATABASE_NAME': 'sendout', 'INSTANCE_NAME': 'sendout-replica', 'DATABASE':'postgres'},
-	{'DATABASE_NAME': 'savings', 'INSTANCE_NAME': 'savings-replica', 'DATABASE':'postgres'},
-	{'DATABASE_NAME': 'assess', 'INSTANCE_NAME': 'assess-replica', 'DATABASE':'assess'},
-	{'DATABASE_NAME': 'ddi', 'INSTANCE_NAME': 'ddi-service-replica', 'DATABASE':'main'}
+	{'DATABASE_NAME': 'main', 'INSTANCE_NAME': 'anyfin-main-replica', 'DATABASE':'main', 'DESTINATION_PROJECT': 'anyfin'},
+	{'DATABASE_NAME': 'dolph', 'INSTANCE_NAME': 'anyfin-dolph-read-replica', 'DATABASE':'postgres', 'DESTINATION_PROJECT': 'anyfin'},
+	{'DATABASE_NAME': 'pfm', 'INSTANCE_NAME': 'pfm-replica', 'DATABASE':'postgres', 'DESTINATION_PROJECT': 'anyfin'},
+	{'DATABASE_NAME': 'psd2', 'INSTANCE_NAME': 'psd2-replica', 'DATABASE':'postgres', 'DESTINATION_PROJECT': 'anyfin'},
+	{'DATABASE_NAME': 'sendout', 'INSTANCE_NAME': 'sendout-replica', 'DATABASE':'postgres', 'DESTINATION_PROJECT': 'anyfin'},
+	{'DATABASE_NAME': 'savings', 'INSTANCE_NAME': 'savings-replica', 'DATABASE':'postgres', 'DESTINATION_PROJECT': 'anyfin'},
+	{'DATABASE_NAME': 'assess', 'INSTANCE_NAME': 'assess-replica', 'DATABASE':'assess', 'DESTINATION_PROJECT': 'anyfin'},
+	{'DATABASE_NAME': 'ddi', 'INSTANCE_NAME': 'ddi-service-replica', 'DATABASE':'main', 'DESTINATION_PROJECT': 'anyfin'},
+	{'DATABASE_NAME': 'main-staging', 'INSTANCE_NAME': 'main-staging-replica', 'DATABASE':'main', 'DESTINATION_PROJECT': 'anyfin-staging'}
 ]
